040423cd7b906e59a0d235a33df97807fd37433f
honggexuan <honggexuan@tuzhanai.com>
2018-07-27 17:24:43 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
         <div>
             <Switch>
                 <Route path="/main" component={Main}/>
-                <Route path="/" component={Navbar}/>
+                <Route path="/" component={Main}/>
             </Switch>
         </div>
     </Router>,
