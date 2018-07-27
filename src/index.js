1843922fc19967c2cdd37e9416f2085d5ed4c818
honggexuan <honggexuan@tuzhanai.com>
2018-07-27 17:29:44 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
         <div>
             <Switch>
                 <Route path="/main" component={Main}/>
-                <Route path="/" component={Main}/>
+                <Route path="/" component={App}/>
             </Switch>
         </div>
     </Router>,
